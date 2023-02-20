7e05d81afc97bf2c19e77e40fa35757939625fb7
Raymond <raymond@philippinedev.com>
2023-02-20 15:34:30 +0800

    relationship validation: categories, products

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'associations' do
     it { should belong_to(:category) }
+    it { should have_many(:orders) }
   end
 end
