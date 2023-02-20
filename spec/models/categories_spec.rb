70a959f5756a5127ec6a2f1d447fc7274614aa70
Raymond <raymond@philippinedev.com>
2023-02-20 15:56:18 +0800

    validate category

--- spec/models/categories_spec.rb
+++ spec/models/categories_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe Category, type: :model do
   describe 'associations' do
     it { should have_many(:products) }
   end
+
+  # Validations
+  let(:name) { 'Dairy' }
+  let(:subject) do
+    build(:category, name: name)
+  end
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  context "when name is empty" do
+    let(:name) { '' }
+    it "is not valid" do
+      expect(subject).to_not be_valid
+    end
+  end
 end
